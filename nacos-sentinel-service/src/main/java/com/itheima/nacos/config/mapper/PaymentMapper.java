c5973ddd596e4c90574cd40b588cfa4602204c2d
zqiqi9857 <zqiqi9857@gmail.com>
2020-07-14 21:24:01 +0800

    git 后端提交2

--- nacos-sentinel-service/src/main/java/com/itheima/nacos/config/mapper/PaymentMapper.java
+++ nacos-sentinel-service/src/main/java/com/itheima/nacos/config/mapper/PaymentMapper.java
@@ -4,9 +4,12 @@ import com.itheima.nacos.config.entity.Payment;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 @Mapper
 public interface PaymentMapper {
     public int create(Payment payment);
     public Payment getPaymentById(@Param("id") Long id);
     public Payment selectByPrimaryKey(@Param("id") Long id);
+    public List<Payment> findAllPayment();
 }
